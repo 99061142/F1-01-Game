0902676da6402ef5d87b8fe1f3ff612dac5ba43f
99061142 <99061142@mydavinci.nl>
2021-10-16 15:00:24 +0200

    Added a decreasement for for the difficulty
    If the user failed more than the max fails of the difficulty, the difficulty gets decreased.
    
    If the user was already at the difficulty "easy", the game stops

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+easy_lost = False # If the user is on the lowest difficulty
+
 # Array for the sums, and the time the user gets
 sum_information = {
     "questions": {
@@ -26,25 +28,39 @@ sum_information = {
         ]
     },
 
-    "time": {
-        "easy": 5,
-        "medium": 15,
-        "hard": 30
+    "max_fails": {
+        "easy": 0,
+        "medium": 0,
+        "hard": 1
     }
 }
 
+difficulties = list ( sum_information['questions'].keys() ) # Makes an array of all the possible difficulties
+
+
+
+
+# Ask the questions to the user
+
 def sum_questions(difficulty):
-    
-    questions = sum_information["questions"][difficulty] # Array with the questions
-    time = sum_information["time"][difficulty] # Time for the user to answer the question
+
+    change_difficulty = False
+
+    questions = sum_information['questions'][difficulty] # Array with the questions
+    max_fails = sum_information['max_fails'][difficulty] # Max fails the user can make for the difficulty
+
+
+    print('This questions are for the difficulty: "' + difficulty + '"')
+
     
     for sum_str in questions:
     
-        question = 'whats "' + sum_str + '" ?: '
-
         user_chosen = False
+
+        question = 'whats "' + sum_str + '" ?: '
     
-        while not user_chosen: 
+        while not user_chosen and not change_difficulty: 
+            
             user_answer = input(question)
 
 
@@ -57,22 +73,61 @@ def sum_questions(difficulty):
             else:
                 user_chosen = True
 
-                answer = eval(sum_str)
+                answer = eval(sum_str) # Calculate the sum
+
 
                 if user_answer == answer:
                     print("Thats correct!")
                 
                 else:
-                    fail_message = "Thats not correct, the answer is: " + str( int(answer) )
+                    fail_message = "That is not correct, the answer was: " + str( int(answer) )
 
                     print(fail_message)
 
 
+                    if max_fails > 0:
+                        max_fails -= 1 # Decrease the fails the user has
 
+                    else: 
+                        try:
+                            difficulty = difficulties.index(difficulty - 1) # Check if the difficulty could be decreased
+                        
+                        except: 
+                            easy_lost = True
+
+                        else: 
+                            easy_lost = False
+
+                        
+                        change_difficulty = True # Go out of the loop with the questions
+
+    else:
+        return easy_lost # Return if the users difficulty must be decreaed
+
+
+
+# Change the difficulty for the questions
+
+def change_difficulty(difficulty, user_lost):
+
+    question_index = difficulties.index(difficulty) # Get the index of the difficulty
+
+    # Check which difficulty the user has, and decrease it
+    
+    if user_lost:
+        difficulty = difficulties[question_index - 1] # Decrease the difficulty
+    
+    else:
+        difficulty = difficulties[question_index + 1] # Increase the difficulty
+
+    return difficulty
+
+
+
+
+# Ask the user which mode he wants to begin with
 
 def startScreen():
-    
-    difficulties = list ( sum_information["questions"].keys() ) # Makes an array of all the keys of the difficulties
 
     difficulties_str = ", ".join(difficulties[:-1]) + " or " + difficulties[-1] # Add a ',' at the end of the value, and if its the last value it adds a 'or'
 
@@ -101,5 +156,16 @@ def startScreen():
     return difficulty
 
 
-difficulty = startScreen()
-sum_questions(difficulty)
+
+
+difficulty = startScreen() # Ask the user which mode he wants to begin with
+
+
+while not easy_lost:
+    user_lost = sum_questions(difficulty) # Ask the questions to the user
+
+    if difficulty != "easy":
+        difficulty = change_difficulty(difficulty, user_lost) # Change the difficulty with the result of the user
+
+    else:
+        easy_lost = True
